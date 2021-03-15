ddbeae9861c74bbd6b2dbb6433bc691e39832219
samane Tohidi <s.tohidi39@gmail.com>
2021-03-15 22:49:08 +0330

    Lesson 66 build a card view

--- Honeymoon/View/ContentView.swift
+++ Honeymoon/View/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+        CardView(honeymoon: honeymoonData[1])
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 11 Pro")
     }
 }
