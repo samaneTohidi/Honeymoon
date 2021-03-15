189c6b11d869b1c085ad437a9b425b2284d4ed13
samane Tohidi <s.tohidi39@gmail.com>
2021-03-16 00:40:57 +0330

    Lesson 68 create a footer view

--- Honeymoon/View/ContentView.swift
+++ Honeymoon/View/ContentView.swift
@@ -21,6 +21,8 @@ struct ContentView: View {
             .padding()
             
             Spacer()
+            
+            FooterView()
 
         }
     }
---
+++ Honeymoon/View/FooterView.swift
@@ -0,0 +1,48 @@
+//
+//  Created by Robert Petras
+//  SwiftUI Masterclass ♥ Better Apps. Less Code.
+//  https://swiftuimasterclass.com 
+//
+
+import SwiftUI
+
+struct FooterView: View {
+ 
+  var body: some View {
+    HStack {
+      Image(systemName: "xmark.circle")
+        .font(.system(size: 42, weight: .light))
+      
+      Spacer()
+      
+      Button(action: {
+        // ACTION
+       
+      }) {
+        Text("Book Destination".uppercased())
+          .font(.system(.subheadline, design: .rounded))
+          .fontWeight(.heavy)
+          .padding(.horizontal, 20)
+          .padding(.vertical, 12)
+          .accentColor(Color.pink)
+          .background(
+            Capsule().stroke(Color.pink, lineWidth: 2)
+          )
+      }
+      
+      Spacer()
+      
+      Image(systemName: "heart.circle")
+        .font(.system(size: 42, weight: .light))
+    }
+    .padding()
+  }
+}
+
+struct FooterView_Previews: PreviewProvider {
+  
+  static var previews: some View {
+    FooterView()
+      .previewLayout(.fixed(width: 375, height: 80))
+  }
+}
